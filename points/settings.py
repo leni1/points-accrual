4589275b8dfbff5a0c0b10720fc98cac3b7ce5fa
Leni Kadali <lenikmutungi@gmail.com>
2019-05-31 20:51:12 +0300

    Add Employee model, SECRET_KEY to points.settings
    Added the Employee model to the points.settings module.
    Added the SECRET_KEY (gotten from environment).

--- points/settings.py
+++ points/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 _FALSE_STRINGS = [
@@ -83,6 +84,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
     'crispy_forms',
 ]
 
@@ -150,6 +152,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# App user model
+AUTH_USER_MODEL = 'app.Employee'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
